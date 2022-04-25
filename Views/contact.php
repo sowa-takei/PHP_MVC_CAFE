88aeed6c53232f2a4cb0301d15d96600d43ca68e
sowa-takei <souwasora@gmail.com>
2022-04-25 15:45:55 +0900

    問い合わせフォーム

--- Views/contact.php
+++ Views/contact.php
@@ -1,5 +1,20 @@
 <?php
-  $error_message = array();
+// require_once('../database.php');
+require_once('../Models/Contact.php');
+require_once('../Controllers/Contact.Controller.php');
+try {
+  $sql = "DELETE FROM contacts WHERE id = :id";
+  $stmt = $dbh->prepare($sql);
+  $params = array(':id'=>$_GET["id"]);
+  $stmt->execute($params);
+
+} catch (Exception $e) {
+        echo 'エラーが発生しました。:' . $e->getMessage();
+}
+
+
+
+$error_message = array();
   if( isset($_POST["btn"] ) && $_POST["btn"] ){
     //エラー項目の確認
     //ニックネームが空の場合
@@ -8,10 +23,10 @@
     } else if( mb_strlen($_POST['name']) > 100 ){
 	    $error_message[] = "名前は10文字以内にしてください";
     }
-    
-    if(!$_POST['name_kana']) {
+
+    if(!$_POST['kana']) {
       $error_message[] = "フリガナを入力してください";
-    } else if( mb_strlen($_POST['name_kana']) > 100 ) {
+    } else if( mb_strlen($_POST['kana']) > 100 ) {
       $error_message[] = "カナは10文字以内にしてください";
     }
     if(! preg_match("/^[0-9]+$/", $tel)){
@@ -26,7 +41,7 @@
       $error_message[] = "問い合わせ内容を記述してください";
     }
   }
-  if (!empty($_POST['name']) && ($_POST['name_kana']) && ($_POST['email']) && ($_POST['content'])){
+  if (!empty($_POST['name']) && ($_POST['kana']) && ($_POST['email']) && ($_POST['content'])){
     header("Location: confirm.php", true, 307);
   }
 ?>
@@ -63,30 +78,63 @@
     <div class="form-row">
       <div class="form-group col-md-7">
         <label>名前<span class="btn btn-danger btn-sm">必須</span></label>
-        <input type="text" name="name" id= "name" class = "form-control" placeholder="例) 水瀬いのり" value="">
+        <input type="text" name="name"  class = "form-control" placeholder="例) 水瀬いのり" value="">
       </div>
       <div class="form-group col-md-7">
         <label>フリガナ<span class="btn btn-danger btn-sm">必須</span></label>
-        <input type="text" name="name_kana" id = "name_kana" class = "form-control" placeholder="例) ミナセイノリ" value="">
+        <input type="text" name="kana"  class = "form-control" placeholder="例) ミナセイノリ" value="">
       </div>
       <div class="form-group col-md-7">
         <label>電話番号<span class="btn btn-danger btn-sm">必須</span></label>
-        <input type="text" name="tel" id = "tel" class = "form-control" placeholder="例) 00000000000" value="">
+        <input type="text" name="tel"  class = "form-control" placeholder="例) 00000000000" value="">
       </div>
       <div class="form-group col-md-7">
         <label>メールアドレス<span class="btn btn-danger btn-sm">必須</span></label>
-        <input type="text" name="email" id = "email" class = "form-control" placeholder="例) sample@sample.com" value="">
+        <input type="text" name="email"  class = "form-control" placeholder="例) sample@sample.com" value="">
       </div>
       <div class="form-group col-7">
         <label>お問い合わせ<span class="btn btn-danger btn-sm">必須</span></label>
-        <textarea name="content" id = "content" class = "form-control" placeholder="お問合せ内容を入力"></textarea>
+        <textarea name="content"  class = "form-control" placeholder="お問合せ内容を入力"></textarea>
       </div>
     </div>
-    <!-- <button type="submit" name= "btn" class="btn btn-success">確認画面へ</button> -->
     <input type="submit" name="btn" class="btn btn-success"value="確認画面へ">
   </form>
   <div>
-</div>
+  <table class="table table-bordered">
+    <tr>
+      <th>id</th>
+      <th>名前</th>
+      <th>フリガナ</th>
+      <th>電話番号</th>
+      <th>メールアドレス</th>
+      <th>お問い合わせ内容</th>
+      <th></th>
+      <th></th>
+    </tr>
+    <?php foreach($result as $column) : ?>
+    <tr>
+      <th><?php echo $column['id'] ?></th>
+      <th><?php echo $column['name'] ?></th>
+      <th><?php echo $column['kana'] ?></th>
+      <th><?php echo $column['tel'] ?></th>
+      <th><?php echo $column['email'] ?></th>
+      <th><?php echo $column['body'] ?></th>
+      <td><a href="../views/edit_form.php?id=<?php echo $column['id'] ?>">編集</td>
+      <td><a href="../views/contact.php?id=<?php echo $column['id'] ?>" onclick="submitChk();">削除</a></td>
+    </tr>
+    <?php endforeach; ?>
+  </table>
+  <script>
+    /**
+     * 確認ダイアログの返り値によりフォーム送信
+    */
+    function submitChk () {
+        /* 確認ダイアログ表示 */
+        var flag = confirm ( "削除してもよろしいですか？\n\n削除したくない場合は[キャンセル]ボタンを押して下さい");
+        /* send_flg が TRUEなら送信、FALSEなら送信しない */
+        return flag;
+    }
+</script>
 </div>
 </body>
 </html>
